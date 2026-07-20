bbc0664c84cfbf8ba32043c5066198c02af5b4ea
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-07-20 09:56:43 -0400

    Use default QA checks
    Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

--- test/qa/qa.jl
+++ test/qa/qa.jl
@@ -3,8 +3,6 @@ using JET
 
 run_qa(
     PubChemReactions;
-    explicit_imports = true,
-    api_docs_kwargs = (; rendered = true),
     jet_kwargs = (; target_defined_modules = true),
     ei_kwargs = (;
         # `escapeuri` is owned by URIs but accessed via HTTP. `scalarize` and
